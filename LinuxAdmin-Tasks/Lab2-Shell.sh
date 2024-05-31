07cf96aefae6751bac36fbc4aab37de639bd40f5
MoYousry510 <Mohammed.yousry510@gmail.com>
2024-05-31 14:15:25 +0300

    DiskSpace script

--- LinuxAdmin-Tasks/Lab2-Shell.sh
+++ LinuxAdmin-Tasks/Lab2-Shell.sh
@@ -4,6 +4,6 @@ FREE=$(df -BG --output=avail / | tail -n1 | tr -d "G"," " )
 
 if [ $FREE -lt 10 ]; then        # if free is less than approximately 10G
 
-  echo "Disk Space Threshold Exceeded, Free Space available is $FREE GB"
+  echo "Disk Space Threshold Exceeded, Free Space available is $FREE GB" | ssmtp Mohammed.yousry510@gmail.com
 
 fi;
